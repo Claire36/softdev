4dfeb2f55cd072e5c01aea10ff47724bec18a207
Claire <csong50@stuy.edu>
2024-09-19 21:31:21 -0400

    added comments to cw 05

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -11,9 +11,12 @@ krewes_list = []
 
 def createList():
     with open("05_bitstream/krewes.txt", "r") as file:
+        #splits the file up into a list so that each person's information is a separate entry
         data = file.read().split("@@@")
         for person in data:
+            #splits every entry in the list into a list of the pieces of information for that person
             info = person.split("$$$")
+            #makes a dictionary of the information for each person and adds it to a big list
             krewes_list.append({"pd": info[0], "devo": info[1], "ducky": info[2]})
 
 def pickDevo():
@@ -22,6 +25,7 @@ def pickDevo():
         print("The list of krewes is empty!")
         return
     l = len(krewes_list) - 1
+    #pick a random index in the list of dictionaries
     choice = random.randint(0, l)
     devo = krewes_list[choice]
     print(devo["devo"] + " " + devo["pd"] + " " + devo["ducky"])
