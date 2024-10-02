02e4fb6ac4f58dc19d1a7ae6b08166800a3c9f2c
Claire <csong50@stuy.edu>
2024-10-02 00:05:35 -0400

    finished work 13

--- 13_combine/app.py
+++ 13_combine/app.py
@@ -1,3 +1,10 @@
+# Claire Song, Sasha Murokh
+# we're cooking
+# SoftDev
+# K13 -- Template for Success
+# 2024-09-30
+# time spent: 1.5
+
 import csv
 import random
 from flask import Flask, render_template
@@ -22,18 +29,17 @@ def occupations():
         csv_reader = csv.reader(file)  # use csv.reader to handle quoted lines bc they exist in our file
         next(csv_reader)  # skip header
         for row in csv_reader:
-            if len(row) == 2:  # check if we have exactly two elements (occupation, percentage)
+            if len(row) == 3:  # check if we have exactly three elements (occupation, percentage, link)
                 occupation = row[0].strip()
                 percentage = float(row[1].strip())
-                occupations[occupation] = percentage
+                occupations[occupation] = [percentage, row[2].strip()]
     
-    input_table = [(k, v) for k, v in occupations.items()]
+    input_table = [(k, v[0]) for k, v in occupations.items()]
     input_table.insert(0,['Job Class','Percentage'])
-    del occupations['Total']
     occupation_list = list(occupations.keys())
-    weights = list(occupations.values())
+    weights = [value[0] for value in occupations.values()]
     selected_occupation = random.choices(occupation_list, weights=weights, k=1)
-    return render_template('tablified.html', randomly_selected=selected_occupation[0], items=input_table)
+    return render_template('tablified.html', randomly_selected=selected_occupation[0], items=input_table, link=occupations[selected_occupation[0]][1])
 
 if __name__ == "__main__":
     app.debug = True
