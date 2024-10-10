e656b05bd796a1ab78e3031ff0c05a07eb6640ca
Claire Song <csong50@stuy.edu>
2024-10-10 11:47:01 -0400

    worked on wk 16

---
+++ 16_flask-sessions/app.py
@@ -0,0 +1,55 @@
+# Chloe Wong, Tiffany Yang, Claire Song
+# Team X
+# SoftDev
+# K15 -- user sessions
+# 2024-10-9
+# Time Spent:
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+from flask import session
+import os
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/") #, methods=['GET', 'POST'])
+def disp_loginpage():
+    #print("\n\n\n")
+    #print("***DIAG: this Flask obj ***")
+    #print(app)
+    #print("***DIAG: request obj ***")
+    #print(request)
+    #print("***DIAG: request.args ***")
+    #print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    #print("***DIAG: request.headers ***")
+    #print(request.headers)
+    return render_template( 'login.html' )
+
+
+@app.route("/auth", methods=['GET', 'POST'])
+def authenticate():
+    #print("\n\n\n")
+    #print("***DIAG: this Flask obj ***")
+    #print(app)
+    #print("***DIAG: request obj ***")
+    #print(request)
+    #print("***DIAG: request.args ***")
+    #print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    #print("***DIAG: request.headers ***")
+    #print(request.headers)
+    app.secret_key = os.urandom(32)
+    print(session)
+    print(request.cookies)
+    return render_template( 'response.html', response = request.args['username'], method = request.method)  #response to a form submission
+
+
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
