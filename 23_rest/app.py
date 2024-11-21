e7fa49bc11478241729adc35ca939a92e7a746ff
Claire <csong50@stuy.edu>
2024-11-20 23:49:41 -0500

    finished work 23

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -1,6 +1,6 @@
 '''
 Claire Song, Tracy Ye
-Everything Bagel
+CTV
 SoftDev
 K23 -- A RESTful Journey Skyward
 2024-11-20
@@ -21,12 +21,10 @@ def api():
     with urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=' + key) as response:
         html = response.read()
         load = json.loads(html)
-        print(html)
-        print("###########")
-        print(load)
-        print("***********")
-        print(json.dumps(load))
-    return render_template("main.html", source=load["url"], explanation=load["explanation"])
+        #print(html)
+        #print(load)
+        #print(json.dumps(load))
+    return render_template("main.html", source=load["url"], explanation=load["explanation"], source2=load["hdurl"])
 
 
 if __name__ == "__main__":
---
+++ 23_rest/readme.md
@@ -0,0 +1,18 @@
+Claire Song, Tracy Ye  
+CTV   
+SoftDev  
+K23 -- A RESTful Journey Skyward -- rest-API & NASA  
+2024-11-20  
+Time Spent: 1
+
+# K23: A RESTful Journey Skyward
+
+## DISCO:
+
+- json.loads() allows us to work with JSON strings in Python.
+- json.dumps() allows us to turn Python objects into JSON strings.
+- URLs can be opened using urllib.request.urlopen().
+
+## QCC:
+
+- What does changing the method do in urllib.request.Request()?
