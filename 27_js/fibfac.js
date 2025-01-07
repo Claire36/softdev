3fd3f2ad73e7cf67a7d8185cd2de2db12a7376b4
Claire <csong50@stuy.edu>
2025-01-06 22:30:23 -0500

    finished work 27

---
+++ 27_js/fibfac.js
@@ -0,0 +1,55 @@
+
+//Team Chip & Dip :: Claire Song, Chloe Wong
+//SoftDev pd5
+//K27 - Basic functions in JavaScript
+//2025-01-06m
+
+//JavaScript implementations of Day0 recursive Scheme functions
+
+//factorial:
+
+//<your team's fact(n) implementation>
+
+let fact = function(n) {
+    if (n==1) {
+        return 1;
+    }
+    else {
+        return n * fact(n-1);
+    }
+}
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+
+fact(3)
+fact(4)
+fact(5)
+
+//-----------------------------------------------------------------
+
+
+//fib:
+
+//<your team's fib(n) implementation>
+
+let fib = function(n) {
+    if (n==0) {
+        return 0;
+    }
+    else if (n==1) {
+        return 1;
+    }
+    else {
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+
+fib(3)
+fib(4)
+fib(5)
+
+//=================================================================
